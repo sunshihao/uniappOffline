1b2cf674933c1046f07a41449ca9fed934dc6780
SSSH <sunshihao945@outlook.com>
2023-11-06 15:10:13 +0800

    fix: 首次提交

---
+++ app/src/main/java/com/sssh/pushdemo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.sssh.pushdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cmtietong.cmttRwfamily.R;
+
+//import com.netease.nimlib.sdk.NIMClient;
+//import com.netease.nimlib.sdk.Observer;
+//import com.netease.nimlib.sdk.lifecycle.SdkLifecycleObserver;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        NIMClient.getService(SdkLifecycleObserver.class).observeMainProcessInitCompleteResult(new Observer<Boolean>() {
+//            @Override
+//            public void onEvent(Boolean aBoolean) {
+//                if (aBoolean != null && aBoolean) {
+//                    // 主进程初始化完毕，可以开始访问数据库
+//
+//                }
+//            }
+//        }, true);
+
+    }
+}
